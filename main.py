7a3b0365d3e5f260c91d55970d2c5ded8dfa7bbe
Ankush Kumar <ankush1382003@gmail.com>
2022-12-06 20:08:58 +0530

    update

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 # This is Email Slicer Code Generator.
 print("Welcome to Email Slicer Generator")
 print("Please Enter Your Email Address Below")
+
 # Here the user will be prompted to enter their email address.
 user_input = input("\tEmail Address: ")
+
 # Here is the email address slicing logic comes in if statement.
 if "@" in user_input:
     print("Your Have Valid Email  address")
@@ -12,5 +14,6 @@ if "@" in user_input:
     domain = user_input[x+1:]
     print("This is Email Username: ", username)
     print("This is Email Domain: ", domain)
+
 else:
     print("Invalid Email Address")
